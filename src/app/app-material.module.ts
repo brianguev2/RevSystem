e1acd60f7fda427df642ba2469b93571422b33b4
Brian Guevara <brianguev2@gmail.com>
2018-10-11 12:00:20 -0400

    Project Structure Setup with npm dependencies

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatSidenavModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule
+  ]
+})
+export class AppMaterialModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+import { AppMaterialModule } from './app-material.module';
+import { AppRoutingModule } from './app-routing.module';
+
+import {
+  HeaderComponent,
+  SharedModule
+} from './shared';
+import { CoreModule } from './core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    AppMaterialModule,
+    AppRoutingModule,
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
